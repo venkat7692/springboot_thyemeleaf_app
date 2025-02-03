d1a14acc8cc50682593cdb019bf23f98ba590cde
venkat <venkatgeethsai@gmail.com>
2025-02-03 20:46:48 +0530

    initial commit

---
+++ src/main/java/in/ashokit/controller/MessageController.java
@@ -0,0 +1,14 @@
+package in.ashokit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MessageController {
+	@GetMapping("/welcome")
+public String getWelcomeMsg(Model model) {
+	model.addAttribute("msg", "welcome to thymeleaf");
+	return "index"; 
+}
+}
---
+++ src/main/java/in/ashokit/controller/ProductController.java
@@ -0,0 +1,26 @@
+package in.ashokit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import in.ashokit.binding.Product;
+
+@Controller
+public class ProductController {
+//method to display the form 
+	@GetMapping("/")
+	public String loadForm(Model model) {
+		model.addAttribute("product", new Product());
+		return "index";
+	}
+	
+	//method to display form submission 
+	@PostMapping("/product")
+	public String handleSubmitBtn(Product p,Model model) {
+		System.out.println(p);
+		model.addAttribute("msg", "product saved ");
+		return "index";
+	}
+}
